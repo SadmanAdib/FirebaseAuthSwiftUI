0c303807adc8c6e2fde6ffd385fabefb25a5de5e
Sadman Adib <sadmanadib67@gmail.com>
2022-08-03 12:10:56 +0600

    AuthUI done

--- FirebaseAuthSwiftUI/ContentView.swift
+++ FirebaseAuthSwiftUI/ContentView.swift
@@ -8,9 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isLoginMode = false
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack {
+                Color((.init(white: 0, alpha: 0.05))).ignoresSafeArea()
+                
+                VStack(spacing: 20) {
+                    Picker("This is picker", selection: $isLoginMode) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }
+                    .pickerStyle(.segmented)
+                    .padding()
+                    
+                    if !isLoginMode{
+                        Button{
+                            
+                        } label: {
+                            ZStack{
+                                Circle()
+                                    .stroke(.blue, lineWidth: 5)
+                                    .frame(width: 120, height: 120)
+                                
+                                Image(systemName: "person.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 60, height: 60)
+                            }
+                        }
+                        .shadow(radius: 5)
+                        
+                        Text("Select a picture")
+                            .font(.caption2)
+                            .foregroundColor(.secondary)
+                        
+                    }
+                    
+                    Form{
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        SecureField("Password", text: $password)
+                    }
+                    .background(.white)
+                    .cornerRadius(15)
+                    .disableAutocorrection(true)
+                    .padding(.vertical, -20)
+                    
+                    Spacer()
+                    
+                    Button{
+                        
+                    } label: {
+                        Text(isLoginMode ? "Log In" : "Create Account")
+                    }
+                    .buttonStyle(.bordered)
+                    .tint(.blue)
+                    .controlSize(.large)
+                    .padding(.bottom, 50)
+                }
+                .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+            }
+        }
     }
 }
 
