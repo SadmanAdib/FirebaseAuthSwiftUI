62e42030b8d0fc3df1c1c4a6ab4d4eb885fe08fe
Sadman Adib <sadmanadib67@gmail.com>
2022-08-03 11:15:02 +0600

    Initial Commit

---
+++ FirebaseAuthSwiftUI/FirebaseAuthSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthSwiftUIApp.swift
+//  FirebaseAuthSwiftUI
+//
+//  Created by Sadman Adib on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
